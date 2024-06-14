081baa3ac343dd638837c1fdc56a8a834cf9fc95
Anshul Garg <anshulgarg789@gmail.com>
2024-06-13 23:50:28 -0500

    generated plots for analytics module

--- src/App/analytics_module.py
+++ src/App/analytics_module.py
@@ -1,49 +1,26 @@
 import pandas as pd
-import plotly.express as px
+import numpy as np
 import streamlit as st
-import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+
 
 def analysis():
+
     st.header('Analytics')
+    st.header("Minimum Flight Duration")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\min_flight_duration.png')
+    st.header("Average Price by Trip Type")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\price_by_trip_type.png')
+    st.header("Average Price by Carrier")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\price_by_carrier.png')
+    st.header("Average Price by Days to Fly")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\days_to_fly_vs_price.png')
+    st.header("Average Price by Round Trip Duration Days")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\price_by_round_trip_duration.png')
+    st.header("Minimum Flight Duration")
+    st.image(r'C:\Users\anshu\Desktop\MLOps\Flight-MLOps-Project\Flight-MLOps-Project\src\visualization\analytics_plots\price_by_weekday.png')
 
-    data = {
-        'airport': ['Los Angeles', 'New York'],
-        'latitude': [34.0522, 40.7128],
-        'longitude': [-118.2437, -74.0060]
-    }
-
-    map_df = pd.DataFrame(data)
-
-    # Separate DataFrame for highlighting specific airports
-    highlight_df = map_df.copy()
-
-    # Set marker size for Los Angeles and New York
-    highlight_df.loc[highlight_df['airport'] == 'Los Angeles', 'marker_size'] = 7  # Smaller size for Los Angeles
-    highlight_df.loc[highlight_df['airport'] == 'New York', 'marker_size'] = 7  # Smaller size for New York
-
-    # Set default marker size for other airports
-    highlight_df['marker_size'].fillna(5, inplace=True)
-
-    fig = px.scatter_mapbox(highlight_df, lat="latitude", lon="longitude",
-                            color_discrete_sequence=["red"],
-                            size='marker_size', zoom=3,
-                            mapbox_style="open-street-map", width=450, height=450, hover_name="airport")  # Adjust width and height
-
-    # Add a line connecting Los Angeles and New York
-    line_trace = go.Scattermapbox(
-        lat=[34.0522, 40.7128],
-        lon=[-118.2437, -74.0060],
-        mode='lines',
-        line=dict(width=2, color='blue'),
-        name='Flight Path'
-    )
-
-    # Add the line trace to the figure
-    fig.add_trace(line_trace)
-
-    st.plotly_chart(fig, use_container_width=True)
-
-    st.text('Hello')
-
-# Run the analysis function
-analysis()
+if __name__ == '__main__':
+    analysis()
